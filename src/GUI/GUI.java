b96288ca3e83665dcab8f1d05ff9cceabed9e655
mlqmn <13523044@std.stei.itb.ac.id>
2025-05-20 09:41:54 +0700

    feat: GUI look and feel

--- src/GUI/GUI.java
+++ src/GUI/GUI.java
@@ -1,7 +1,18 @@
 package src.GUI;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class GUI {
     public static void start(){
+        try{
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException e){
+        } catch (ClassNotFoundException e) {
+        } catch (InstantiationException e) {
+        } catch (IllegalAccessException e) {
+        }
+        
         MainFrame _ = new MainFrame();
     }
 }
